9590c5d02397d5ce7f5864f4c8bce11799b5a68b
Harsha <harsha.honasoge@gmail.com>
2017-01-22 23:53:30 -0800

    This seems to work

--- ebdjango/ebdjango/settings.py
+++ ebdjango/ebdjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!o9rvv!$z+1l%_a+d8tadk+2u*h#)mhds_zx&0%5xx_+=&l^va'
+SECRET_KEY = 'w+-pt7uzw7p4tb@l$-s8!f6jy6ojn5)rqa(%^c&#8ymkwnvn!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'test.apps.TestConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -107,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Pacific'
 
 USE_I18N = True
 
